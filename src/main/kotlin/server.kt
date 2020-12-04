77ef208e380bffcb6a6d9e0354d3fe7bdfe921cd
Nikita Pochaev <pochaev.nik@gmail.com>
2020-12-04 22:27:13 +0300

    Other way of Heroku deploy

--- src/main/kotlin/server.kt
+++ src/main/kotlin/server.kt
@@ -1,40 +1,59 @@
 import io.ktor.application.*
-import io.ktor.features.*
-import io.ktor.gson.*
-import io.ktor.html.respondHtml
+//import io.ktor.features.*
+//import io.ktor.gson.*
+//import io.ktor.html.respondHtml
 import io.ktor.http.*
 import io.ktor.response.*
-import io.ktor.routing.get
-import io.ktor.routing.routing
+import io.ktor.routing.*
+//import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import kotlinx.html.*
+//import kotlinx.html.*
 
-fun HTML.index() {
-    head {
-        title("Hello from Ktor!")
-    }
-    body {
-        div {
-            +"Hello from Ktor. Test message."
-        }
-    }
-}
+//fun HTML.index() {
+//    head {
+//        title("Hello from Ktor!")
+//    }
+//    body {
+//        div {
+//            +"Hello from Ktor. Test message."
+//        }
+//    }
+//}
+//
+//fun main() {
+//    val port = Integer.valueOf(System.getenv("PORT"))
+//    embeddedServer(Netty, port) {
+//        install(ContentNegotiation) {
+//            gson {
+//                setPrettyPrinting()
+//            }
+//        }
+//        routing {
+//            get("/") {
+//                call.respondHtml(HttpStatusCode.OK, HTML::index)
+//            }
+//            get("hello") {
+//                call.respond(HttpStatusCode.Accepted, "Hello")
+//            }
+//        }
+//    }.start(wait = true)
+//}
 
 fun main() {
+    val jsonResponse = """{
+        "id": 1,
+        "task": "Pay waterbill",
+        "description": "Pay water bill today",
+    }"""
     val port = Integer.valueOf(System.getenv("PORT"))
     embeddedServer(Netty, port) {
-        install(ContentNegotiation) {
-            gson {
-                setPrettyPrinting()
-            }
-        }
-        routing {
+        install(Routing) {
             get("/") {
-                call.respondHtml(HttpStatusCode.OK, HTML::index)
+                call.respondText("Hello World!", ContentType.Text.Plain)
             }
-            get("hello") {
-                call.respond(HttpStatusCode.Accepted, "Hello")
+            get("/todo") {
+                call.respondText(jsonResponse, ContentType.Application.Json)
             }
         }
     }.start(wait = true)
