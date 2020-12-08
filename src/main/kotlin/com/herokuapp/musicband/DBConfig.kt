8d05c7ac857ee746bcd3685be6444453361245d5
Nikita Pochaev <pochaev.nik@gmail.com>
2020-12-08 12:57:41 +0300

    BD work

--- src/main/kotlin/com/herokuapp/musicband/DBConfig.kt
+++ src/main/kotlin/com/herokuapp/musicband/DBConfig.kt
@@ -7,6 +7,8 @@ import io.ktor.application.*
 import io.ktor.util.*
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.addLogger
 import org.jetbrains.exposed.sql.transactions.transaction
 
 const val HIKARI_CONFIG_KEY = "ktor.hikariconfig"
@@ -14,10 +16,12 @@ const val HIKARI_CONFIG_KEY = "ktor.hikariconfig"
 @KtorExperimentalAPI
 fun Application.initDB() {
     val configPath = environment.config.property(HIKARI_CONFIG_KEY).getString()
-    println("DB configuration path: $configPath")
     val dbConfig = HikariConfig(configPath)
     val dataSource = HikariDataSource(dbConfig)
     Database.connect(dataSource)
+    transaction {
+        addLogger(StdOutSqlLogger)
+    }
     createTables()
     println("Initialized Database")
 }
