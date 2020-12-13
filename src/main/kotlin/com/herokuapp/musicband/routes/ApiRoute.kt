0af4a7f43f3bfad59bada8584e33137cd972251c
Nikita Pochaev <pochaev.nik@gmail.com>
2020-12-13 22:45:47 +0300

    Connect routing for static files

--- src/main/kotlin/com/herokuapp/musicband/routes/ApiRoute.kt
+++ src/main/kotlin/com/herokuapp/musicband/routes/ApiRoute.kt
@@ -4,6 +4,9 @@ import io.ktor.routing.Routing
 import io.ktor.routing.route
 
 fun Routing.apiRoute() {
+    route("/") {
+        routeFilesystem()
+    }
     route("/api/v1") {
         default()
         groups()
