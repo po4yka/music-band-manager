5bce2e68e7898682014ae47b93baf13121b068f4
Nikita Pochaev <pochaev.nik@gmail.com>
2020-12-13 22:30:43 +0300

    update paths

--- src/main/kotlin/com/herokuapp/musicband/pages/Index.kt
+++ src/main/kotlin/com/herokuapp/musicband/pages/Index.kt
@@ -13,8 +13,8 @@ import kotlinx.html.title
 fun HTML.index() {
     head {
         title("Music Band")
-        styleLink("/resources/frontend/css/style.css")
-        script(src = "/resources/frontend/js/script.js") {}
+        styleLink("resources/frontend/css/style.css")
+        script(src = "resources/frontend/js/script.js") {}
     }
     body {
         h1 {
