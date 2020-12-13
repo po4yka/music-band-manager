16a2b1d248d3c0203f487f9106d29f7c792634f7
Nikita Pochaev <pochaev.nik@gmail.com>
2020-12-13 19:53:03 +0300

    Start creating frontend main page

---
+++ src/main/kotlin/com/herokuapp/musicband/pages/Index.kt
@@ -0,0 +1,38 @@
+package com.herokuapp.musicband.pages
+
+import kotlinx.html.HTML
+import kotlinx.html.a
+import kotlinx.html.body
+import kotlinx.html.div
+import kotlinx.html.h1
+import kotlinx.html.head
+import kotlinx.html.script
+import kotlinx.html.styleLink
+import kotlinx.html.title
+
+fun HTML.index() {
+    head {
+        title("Music Band")
+        styleLink("/resources/css/style.css")
+        script(src = "/resources/script.js") {}
+    }
+    body {
+        h1 {
+            +"Welcome, to the music band manager database"
+        }
+        div(classes = "topnav") {
+            a(classes = "active") {
+                href = "#home"
+                +"Home"
+            }
+            a {
+                href = "#news"
+                +"News"
+            }
+            a {
+                href = "#contact"
+                +"Contact"
+            }
+        }
+    }
+}
--- src/main/kotlin/com/herokuapp/musicband/routes/DefaultRoute.kt
+++ src/main/kotlin/com/herokuapp/musicband/routes/DefaultRoute.kt
@@ -1,26 +1,12 @@
 package com.herokuapp.musicband.routes
 
+import com.herokuapp.musicband.pages.index
 import io.ktor.application.call
 import io.ktor.html.respondHtml
 import io.ktor.http.HttpStatusCode
 import io.ktor.routing.Route
 import io.ktor.routing.get
 import kotlinx.html.HTML
-import kotlinx.html.body
-import kotlinx.html.div
-import kotlinx.html.head
-import kotlinx.html.title
-
-fun HTML.index() {
-    head {
-        title("Hello from Ktor!")
-    }
-    body {
-        div {
-            +"Hello from Ktor. Test message."
-        }
-    }
-}
 
 fun Route.default() {
     get("/") {
