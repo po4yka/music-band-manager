29950a772ceed7fd98e62c7692cd1b46da697721
Nikita Pochaev <pochaev.nik@gmail.com>
2020-12-19 13:05:58 +0300

    Try to return group name for performer

--- src/main/kotlin/com/herokuapp/musicband/data/Performer.kt
+++ src/main/kotlin/com/herokuapp/musicband/data/Performer.kt
@@ -6,11 +6,12 @@ import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.`java-time`.date
+import org.jetbrains.exposed.sql.select
 
 object Performers : IntIdTable() {
     val fullName = varchar("full_name", 255)
     val birthday = date("birthday")
-    val groupId = integer("group_id")
+    val residentialGroupId = optReference("group_id", Groups)
     val role = varchar("role", 255)
 }
 
@@ -24,18 +25,23 @@ class PerformerEntity(id: EntityID<Int>) : IntEntity(id) {
     // corresponding columns of the database table
     var fullName by Performers.fullName
     var birthday by Performers.birthday
-    var groupId by Performers.groupId
+    var residentialGroupId by Performers.residentialGroupId
     var role by Performers.role
 
-    override fun toString(): String = "Performer($fullName, $birthday, $groupId, $role)"
+    override fun toString(): String = "Performer($fullName, $birthday, $residentialGroupId, $role)"
 
     // transform Entity to a simple Kotlin data class
-    fun toPerformer() = Performer(fullName, birthday, groupId, role)
+    fun toPerformer() {
+        val rowId: Int = residentialGroupId?.value ?: -1
+        val groupName = Groups.select { Groups.id eq rowId }.
+                        withDistinct().map { it[Groups.groupName] }[0]
+        Performer(fullName, birthday, groupName, role)
+    }
 }
 
 data class Performer(
     val fullName: String,
     val birthday: LocalDate,
-    val groupId: Int,
+    val groupName: String,
     val role: String
 )
