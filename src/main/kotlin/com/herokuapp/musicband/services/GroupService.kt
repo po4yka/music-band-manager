6af878d85b132388bac9b91c32d026d0208d4523
Nikita Pochaev <pochaev.nik@gmail.com>
2020-12-22 02:14:03 +0300

    sort groups descending order

--- src/main/kotlin/com/herokuapp/musicband/services/GroupService.kt
+++ src/main/kotlin/com/herokuapp/musicband/services/GroupService.kt
@@ -2,12 +2,14 @@ package com.herokuapp.musicband.services
 
 import com.herokuapp.musicband.data.Group
 import com.herokuapp.musicband.data.GroupEntity
+import com.herokuapp.musicband.data.Groups
+import org.jetbrains.exposed.sql.SortOrder
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class GroupService {
 
     fun getAllGroups(): Iterable<Group> = transaction {
-        GroupEntity.all().map(GroupEntity::toGroup)
+        GroupEntity.all().orderBy(Groups.hitParadePlace to SortOrder.DESC).map(GroupEntity::toGroup)
     }
 
     fun addGroup(group: Group) {
