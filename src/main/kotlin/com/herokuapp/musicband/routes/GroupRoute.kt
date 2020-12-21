343fffc4720a046d59ee3e1a6b1d8ee810257949
Nikita Pochaev <pochaev.nik@gmail.com>
2020-12-21 12:16:10 +0300

    fix lint)))

--- src/main/kotlin/com/herokuapp/musicband/routes/GroupRoute.kt
+++ src/main/kotlin/com/herokuapp/musicband/routes/GroupRoute.kt
@@ -1,6 +1,5 @@
 package com.herokuapp.musicband.routes
 
-import com.herokuapp.musicband.data.Group
 import com.herokuapp.musicband.services.GroupService
 import io.ktor.application.call
 import io.ktor.features.NotFoundException
