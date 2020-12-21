7c0bffa3cfd4af564ba0c7518f6a471f213b1e15
Nikita Pochaev <pochaev.nik@gmail.com>
2020-12-21 15:32:53 +0300

    fix lint

--- src/main/kotlin/com/herokuapp/musicband/util.kt
+++ src/main/kotlin/com/herokuapp/musicband/util.kt
@@ -1,7 +1,5 @@
 package com.herokuapp.musicband
 
-import kotlin.text.Regex.Companion.escapeReplacement
-
 fun removeQuotesAndUnescape(uncleanJson: String): String {
     val noQuotes: String = uncleanJson.replace("^\"|\"$".toRegex(), "")
     return noQuotes.replace("""\"""", """"""")
